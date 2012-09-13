f928b1ccc4f41a820343aed5d83c0c661a92068a
Scott King <scott_king@taylor.edu>
2012-09-13 11:08:35 -0400

    Finish exercise 1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,19 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+  
+  describe "Contact page" do
+	
+	it "should have the h1 'Contact Us'" do
+		visit '/static_pages/contact'
+		page.should have_selector('h1', :text => 'Contact Us')
+	end
+	
+	it "should have the title 'Contact Us'" do
+		visit '/static_pages/contact'
+		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+	end
+  end
+		
+  
 end
